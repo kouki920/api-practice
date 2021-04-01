11a7e5d475cda907071aacbd82d88da730967d49
kouki920 <kouki.htt.920@gmail.com>
2021-04-01 22:15:05 +0900

    [update]APIの取得コードの修正

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -4,39 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use SebastianBergmann\Environment\Console;
 
 class TestController extends Controller
 {
-    //     public function index()
-    //     {
-    //         //api設定
-    //         $tag_id = "Git";
-    //         $url = "https://qiita.com/api/v2/tags/" . $tag_id . "/items?page=1&per_page=20";
-    //         $method = "GET";
-
-    //         //接続
-    //         $client = new Client();
-    //         $response = $client->request($method, $url);
-
-    //         $items = $response->getBody();
-    //         $posts = json_decode($items, true);
-    //         return view('index', compact('posts'));
-    //     }
     public function index()
     {
-        $tag_id = "laravel";
-
+        //api設定
+        $tag_id = "PHP";
         $url = "https://qiita.com/api/v2/tags/" . $tag_id . "/items?page=1&per_page=20";
         $method = "GET";
 
         //接続
         $client = new Client();
-
         $response = $client->request($method, $url);
 
-        $posts = $response->getBody();
-        $posts = json_decode($posts, true);
-
-        return view('index', ['posts' => $posts]);
+        //apiのデータを取得、json形式を配列に変換
+        $results = $response->getBody();
+        $posts = json_decode($results, true);
+        return view('index', compact('posts'));
     }
 }
